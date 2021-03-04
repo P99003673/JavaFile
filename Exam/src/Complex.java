d70d2701c528ff196ae3b94abd982635b3f100fc
P99003673 <78849897+P99003673@users.noreply.github.com>
2021-03-04 18:27:26 +0530

    Update Complex.java

--- Exam/src/Complex.java
+++ Exam/src/Complex.java
@@ -1,19 +1,19 @@
-class Complex {
-	private String name, skill, nl; 
-   Complex(String name, String skill, String nl){
+class Comp {
+	private String name, skill, nat; 
+   Comp(String name, String skill, String nat){
       this.name = name;
       this.skill = skill;
-      this.nl = nl;
+      this.nat = nat;
    }
-   public boolean equals(Object obj) {
-      if (obj == this) {
+   public boolean equals(Object o) {
+      if (o == this) {
          return true;
       }
-      if (!(obj instanceof Complex)) {
+      if (!(o instanceof Comp)) {
          return false;
       }
-      Complex emp = (Complex) obj;
-      return name.equals(emp.name)&& skill.equals(emp.skill)  && nl.equals(emp.nl);
+      Comp emp = (Comp) o;
+      return name.equals(emp.name)&& skill.equals(emp.skill)  && nat.equals(emp.nat);
    }
 }
 
